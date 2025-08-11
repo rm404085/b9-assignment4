38b1c3ef31bc91186207c4d68f92b1e05d90d92a
rm40405 <rm404085@gmail.com>
2025-08-11 10:09:04 +0600

    first commit

--- problem.js
+++ problem.js
@@ -1,16 +1,24 @@
 // problem 5
 function monthlySavings(arr , livingCost) {
     // You have to write your code here
-    let sum = 0;
+    if(Array.isArray(arr)===false || typeof arr!=="number"){
+        return "Invalid input";
+    }
+    let totalCost = 0;
     for(const array of arr){
-         if(array >= 3000){
-            sum = sum+array;
-            let discount = 0.2;
-            let savings =livingCost - (sum + discount) ;
-            return  savings;
+        if(array >=3000){
+            totalCost = array * 0.2;
+
+        }
+    
+          totalCost += array;
+          
+        
          }
+         return totalCost;
+         
     }
     
       
-}
+
 console.log(monthlySavings([ 1000 , 2000 , 3000 ] , 5400 ));
